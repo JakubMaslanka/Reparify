127d07070355e3f1a519a98434b02f160050b2b7
unknown <kuba.ath51@gmail.com>
2022-01-24 19:55:39 +0100

    Getting rid of Styled Components from applications
        - Replace Styled Components, with regular Tailwind CSS classes in
        auth components.
        - Add media breakpoints in Tailwind CSS to keep the application
        responsive on small, medium, and large displays.

--- client/src/components/Auth/AuthInput.tsx
+++ client/src/components/Auth/AuthInput.tsx
@@ -1,6 +1,4 @@
 import { forwardRef } from "react";
-import styled from "styled-components";
-import tw from "twin.macro";
 
 interface AuthInputProps {
     disabled: boolean
@@ -13,70 +11,30 @@ interface AuthInputProps {
 }
 
 export const AuthInput = forwardRef<HTMLInputElement, AuthInputProps>(({
-    disabled, type, error, successIcon, failureIcon, placeholder, id
+    disabled,
+    type,
+    error,
+    successIcon,
+    failureIcon,
+    placeholder,
+    id
 }, ref) => (
     <>
         {error && error.message.includes(type) ? (
-            <InputContainer>
-                <label>{error.message}</label>
+            <div className="flex flex-col pt-4">
+                <label className="text-danger-dark font-semibold">{error.message}</label>
                 <div className="flex relative">
-                    <span>{failureIcon}</span>
-                    <StyledInput ref={ref} disabled={disabled} required type={type} id={id} placeholder={placeholder} />
+                    <span className="px-3 inline-flex items-center shadow-sm text-sm border-t border-l border-b bg-gray-700 border-gray-500 text-gray-500">{failureIcon}</span>
+                    <input className="flex-1 appearance-none border border-gray-500 w-full py-2 px-4 bg-gray-800 text-white-700 placeholder-gray-400 shadow-sm text-base focus:border-greenish-light focus:outline-none" ref={ref} disabled={disabled} required type={type} id={id} placeholder={placeholder} />
                 </div>
-            </InputContainer>
+            </div>
         ) : (
-            <InputContainer>
+            <div className="flex flex-col pt-4">
                 <div className="flex relative">
-                    <span>{successIcon}</span>
-                    <StyledInput ref={ref} disabled={disabled} required type={type} id={id} placeholder={placeholder} />
+                    <span className="px-3 inline-flex items-center shadow-sm text-sm border-t border-l border-b bg-gray-700 border-gray-500 text-gray-500">{successIcon}</span>
+                    <input className="flex-1 appearance-none border border-gray-500 w-full py-2 px-4 bg-gray-800 text-white-700 placeholder-gray-400 shadow-sm text-base focus:border-greenish-light focus:outline-none" ref={ref} disabled={disabled} required type={type} id={id} placeholder={placeholder} autoComplete="on" minLength={type === 'password' ? 6 : 0} />
                 </div>
-            </InputContainer>
+            </div>
         )}
     </>
 ));
-
-const InputContainer = styled.div`
-    ${tw`
-        flex
-        flex-col
-        pt-4
-    `}
-    span {
-        ${tw`
-            px-3
-            inline-flex
-            items-center
-            shadow-sm
-            text-sm
-            border-t
-            border-l
-            border-b
-            bg-gray-700
-            border-gray-500
-            text-gray-500
-        `}
-    }
-    label {
-        ${tw`
-            text-danger-dark
-            font-semibold
-        `}
-    }
-`;
-const StyledInput = styled.input`
-    ${tw`
-        flex-1 
-        appearance-none 
-        border 
-        border-gray-500 
-        w-full 
-        py-2 
-        px-4 
-        bg-gray-800 
-        text-white-700 
-        placeholder-gray-400 
-        shadow-sm text-base 
-        focus:border-berry-darker 
-        focus:outline-none
-    `}        
-`;
